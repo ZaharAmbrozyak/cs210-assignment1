88f16644161ef1b5eaa6872f123b24b8581de3ec
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-03-11 10:08:33 +0200

    Made Tokenizer class

--- cs210-assignment1/ShuntingYard.cs
+++ cs210-assignment1/ShuntingYard.cs
@@ -1,70 +1,8 @@
 namespace cs210_assignment1;
 
-public class ShuntingYard
+public class ShuntingYard : Tokenizer
 {
     private readonly Dictionary<string, double> _memory = new();
-    private Lexer GetLexer(string expression)
-    {
-        var lexer = new Lexer(expression.Length + 1);
-        var i = 0;
-        while (i < expression.Length)
-        {
-            var symbol = expression[i];
-
-            if (char.IsWhiteSpace(symbol))
-            {
-                i++;
-            }
-            else if (char.IsDigit(symbol))
-            {
-                var number = "";
-
-                while (i < expression.Length)
-                {
-                    if (char.IsDigit(expression[i]) || expression[i] == ',')
-                    {
-                        number += expression[i]; 
-                    }
-                    else if (expression[i] == '.')
-                    {
-                        number += ",";
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    i++;
-                }
-
-                lexer.Add(new NumberToken(double.Parse(number)));
-            }
-            else if (char.IsLetter(symbol))
-            {
-                var word = "";
-                while (i < expression.Length && char.IsLetter(expression[i]))
-                {
-                    word += expression[i];
-                    i++;
-                }
-
-                if (i < expression.Length && expression[i] == '(')
-                {
-                    lexer.Add(new FunctionToken(word));
-                }
-                else
-                {
-                    lexer.Add(new VariableToken(word));
-                }
-            }
-            else
-            {
-                lexer.Add(new OperationToken(symbol.ToString()));
-                i++;
-            }
-        }
-        lexer.Add(new EofToken());
-        return lexer;
-    }
     private PostfixExpression GetPostfix(Lexer lexer)
     {
         var postfix = new PostfixExpression(lexer.Size + 1);
---
+++ cs210-assignment1/Tokenizer.cs
@@ -0,0 +1,67 @@
+namespace cs210_assignment1;
+
+public class Tokenizer
+{
+    protected Lexer GetLexer(string expression)
+    {
+        var lexer = new Lexer(expression.Length + 1);
+        var i = 0;
+        while (i < expression.Length)
+        {
+            var symbol = expression[i];
+
+            if (char.IsWhiteSpace(symbol))
+            {
+                i++;
+            }
+            else if (char.IsDigit(symbol))
+            {
+                var number = "";
+
+                while (i < expression.Length)
+                {
+                    if (char.IsDigit(expression[i]) || (expression[i] == ',' && char.IsDigit(expression[i + 1])))
+                    {
+                        number += expression[i]; 
+                    }
+                    else if (expression[i] == '.')
+                    {
+                        number += ",";
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    i++;
+                }
+
+                lexer.Add(new NumberToken(double.Parse(number)));
+            }
+            else if (char.IsLetter(symbol))
+            {
+                var word = "";
+                while (i < expression.Length && char.IsLetter(expression[i]))
+                {
+                    word += expression[i];
+                    i++;
+                }
+
+                if (i < expression.Length && expression[i] == '(')
+                {
+                    lexer.Add(new FunctionToken(word));
+                }
+                else
+                {
+                    lexer.Add(new VariableToken(word));
+                }
+            }
+            else
+            {
+                lexer.Add(new OperationToken(symbol.ToString()));
+                i++;
+            }
+        }
+        lexer.Add(new EofToken());
+        return lexer;
+    }
+}
